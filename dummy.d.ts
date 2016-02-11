197e5b25d98b51791f6b3079b257fc796f0cf93a
Tom GUILLERMIN <tom.guillermin@gmail.com>
2016-02-11 15:31:13 +0100

    Added dummy typings file just to enable dependencies resolution

---
+++ dummy.d.ts
@@ -0,0 +1,5 @@
+
+declare var DUMMY: number;
+
+/*import * as API from "./index";
+export default API;*/
--- index.d.ts
+++
@@ -1,7 +0,0 @@
-
-/*declare module "typescript-game-engine-server" {
-
-}*/
-
-import * as API from "./index";
-export default API;
